899742bb192063ee80f8a8c628667577967e7e95
unknown <aziz706797@gmail.com>
2024-07-07 15:40:51 +0500

    create expenseItem

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:expense_tracker_app/widgets/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MaterialApp( debugShowCheckedModeBanner: false,
+    home: Expenses(),
+  ));
+}
+
